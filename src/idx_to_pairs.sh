fe69a632f38fc8d174b2e1a72d3560a6f841f4b3
Kenneth Church <kwchurch@us.ibm.com>
2023-06-04 14:38:04 -0400

    nothing special

--- src/idx_to_pairs.sh
+++ src/idx_to_pairs.sh
@@ -1,11 +1,26 @@
 #!/bin/sh
 
-N=$1
+dir=$1
+batchsize=$2
+offset=$3
+tmpdir=$4.$$
 
-$JSALTsrc/C/x_to_y La |
+mkdir -p $tmpdir
+
+$JSALTsrc/C/x_to_y La  |
 awk 'BEGIN {FS=OFS="\t"}; 
-      {for(i in ring_buffer) print ring_buffer[i]+0, $1}
-      {ring_buffer[NR % N] = $1}' N=$N | 
-sort -u -T . -S '90%' 
+      {for(i in ring_buffer) printf "%09d\t%09d\n", ring_buffer[i]+0, $1}
+      {ring_buffer[NR % N] = $1}' N=$offset | 
+split -l $batchsize - $tmpdir/x.
+
+for f in $tmpdir/x.*
+do
+sort -u -T . -S '90%' $f |
+$JSALTsrc/C/pairs_to_cos --dir $dir --input_new_pairs
+done
+
+rm -rf $tmpdir
+
+
 
 
