d10115e5e5dc7ac9d12bd2fbd5eadd9c711afeba
Kenneth Church <kwchurch@us.ibm.com>
2023-06-23 12:01:59 -0400

    more examples

---
+++ examples/similar_documents/minigpt4/do_it.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+for id in `cat ids.txt`
+do
+mkdir -p $id
+echo $id | $JSALTsrc/fetch_references_and_citations.py > $id/one_hop.txt
+$JSALTsrc/near.sh $id 20 | $JSALTsrc/tsv_to_html.sh > $id/$id.html
+done
