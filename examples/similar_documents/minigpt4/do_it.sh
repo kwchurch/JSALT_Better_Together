b556cb274a643a5ed289128a247d25ce37b5f63f
Kenneth Church <kwchurch@us.ibm.com>
2023-06-23 12:05:55 -0400

    minigpt4 examples

--- examples/similar_documents/minigpt4/do_it.sh
+++ examples/similar_documents/minigpt4/do_it.sh
@@ -4,5 +4,5 @@ for id in `cat ids.txt`
 do
 mkdir -p $id
 echo $id | $JSALTsrc/fetch_references_and_citations.py > $id/one_hop.txt
-$JSALTsrc/near.sh $id 20 | $JSALTsrc/tsv_to_html.sh > $id/$id.html
+$JSALTsrc/near.sh $id 20 | $JSALTsrc/tsv_to_html.sh > $id/$id.md
 done
