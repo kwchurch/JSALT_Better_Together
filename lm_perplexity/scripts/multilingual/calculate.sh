30bd76445167d8ca69996d3d22e183c5ff93d608
abteen <abteen.ebrahimi@gmail.com>
2023-07-21 08:15:08 -0600

    scripts

--- lm_perplexity/scripts/multilingual/calculate.sh
+++ lm_perplexity/scripts/multilingual/calculate.sh
@@ -1,21 +1,21 @@
 #!/bin/bash
 
-for model in allenai/scibert_scivocab_cased; do
+for model in xlm-roberta-base; do
 	
-	for bin in {0..45..10} 99; do
+	for bin in {0..99..10} 99; do
 
 		echo ${bin} ${model}
 		
 		python convert_torch_to_np.py \
 			--bin_num ${bin} \
 			--model_name ${model} \
-			--experiment_name filter_newline \
+			--experiment_name multilingual \
 			--sample_size 1000
 	done
 
 	python bin_summary_stats.py \
 		--model_name ${model} \
-		--experiment_name filter_newline \
+		--experiment_name multilingual \
 		--sample_size 1000
 
 	done
