7d638739e5be86244b11ed93d68a7b2b79523a92
abteen <abteen.ebrahimi@gmail.com>
2023-07-21 05:21:13 -0600

    add scripts for newline experiment

--- lm_perplexity/scripts/filter_newline/score.sh
+++ lm_perplexity/scripts/filter_newline/score.sh
@@ -3,12 +3,10 @@
 for model in bert-base-cased roberta-base allenai/scibert_scivocab_cased; do
 	
 	for bin in {0..99..5} 99; do
-	echo ${bin} ${model}
+
 	python optim_score_bert_large.py \
 		--bin_num ${bin} \
 		--model_name ${model} \
 		--experiment_name filter_newline \
-		--strip_newlines \
 		--sample_size 1000 
-	
 	done; done
