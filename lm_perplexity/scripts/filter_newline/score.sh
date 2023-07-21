fd018a74bd0055c4fabd3715461876cb664c0076
abteen <abteen.ebrahimi@gmail.com>
2023-07-21 05:29:13 -0600

    cleanup

--- lm_perplexity/scripts/filter_newline/score.sh
+++ lm_perplexity/scripts/filter_newline/score.sh
@@ -2,11 +2,15 @@
 
 for model in bert-base-cased roberta-base allenai/scibert_scivocab_cased; do
 	
-	for bin in {0..99..5} 99; do
+	for bin in {0..99..10} 99; do
 
+	echo ${bin} ${model}
+	
 	python optim_score_bert_large.py \
 		--bin_num ${bin} \
 		--model_name ${model} \
 		--experiment_name filter_newline \
+		--strip_newlines \
 		--sample_size 1000 
+	
 	done; done
