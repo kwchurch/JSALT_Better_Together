3d2d44c50b748e45139efc3704a2f60b258a1185
petervickers <ptrvickers@gmail.com>
2023-08-18 15:42:25 +0100

    Update forecasting code
    Update forecasting code to match plots used in final presentation

--- src/forecasting/Predicting_from_Random_Walks.py
+++ src/forecasting/Predicting_from_Random_Walks.py
@@ -1,11 +1,6 @@
 #!/usr/bin/env python
 # coding: utf-8
 
-# For each method get Results Matrix R
-## For each model trained_upto_bin_k (M_0)
-### Find the optimal descision threshold with LogisticRegression on walk.aa to maximise accuracy for predicting 1 vs 2-4 hops
-### For each bin collect all the walks where the larger value is in that bin (M_1)
-#### Calcuate the accuracy ACC and Jouden's J of predicting 1 vs 2-4 hops for that bin 
 
 import os
 import numpy as np
@@ -53,20 +48,34 @@ def youdens_j_score(y_true, y_pred):
     specificity = tn / (tn + fp)
     return sensitivity + specificity - 1
 
-
-def test_acc_from_threshold(full_test_df, threshold, prior_class_1_prob):
-    # apply threshold as class prediciton (1 vs 2-4 hops)
+FORECAST_BINS = 2 
+def test_acc_from_threshold(full_test_df, threshold, prior_class_1_prob, test_train_split_bin, specterv1_test_df, 
+                            specterv1_threshold, fallback_method):
+    # ProNE: apply threshold as class prediciton (1 vs 2-4 hops)
     full_test_df['pred'] = (full_test_df['cos_sim'] >= threshold)
     full_test_df['pred'] = full_test_df['pred'].astype(int)
     
-    full_test_df['prior_guess'] = np.random.binomial(1, prior_class_1_prob, size=len(full_test_df))
+    # Specter: apply threshold as class prediciton (1 vs 2-4 hops)
+    specterv1_test_df['pred'] = (specterv1_test_df['cos_sim'] >= specterv1_threshold)
+    specterv1_test_df['pred'] = specterv1_test_df['pred'].astype(int)
     
+    
+    # Fallback for missing values
     full_test_df['no_data'] = full_test_df['cos_sim'] == -1
     
-    # Replace values in 'pred' column with 'prior_guess' where 'no_data' is True, otherwise keep the existing 'pred' values
-    full_test_df['pred'] = np.where(full_test_df['no_data'], full_test_df['prior_guess'], full_test_df['pred'])
+    if fallback_method == 'specter1':
+        # Replace values in 'pred' column with 'specterv1' where 'no_data' is True, otherwise keep the existing 'pred' values
+        full_test_df['pred'] = np.where(full_test_df['no_data'], specterv1_test_df['pred'], full_test_df['pred'])
 
-    #count_cos_sim_minus_1 = full_test_df[full_test_df['cos_sim'] == -1].groupby(full_test_df[['bin0', 'bin1']].max(axis=1)).size().to_dict()
+    elif fallback_method == 'guess':
+        # Replace values in 'pred' column with 'prior_guess' where 'no_data' is stil True, otherwise keep the existing 'pred' values  
+        full_test_df['prior_guess'] = np.random.binomial(1, prior_class_1_prob, size=len(full_test_df))
+        full_test_df['pred'] = np.where(full_test_df['no_data'], full_test_df['prior_guess'], full_test_df['pred'])
+
+    else:
+        print(f'fallback method {fallback_method} is invalid, exiting...')
+        raise NotImplementedError
+    
 
     # Group the DataFrame by the maximum value between 'bin0' and 'bin1'
     # Then count occurrences of cos_sim == -1 and calculate the proportion
@@ -76,8 +85,14 @@ def test_acc_from_threshold(full_test_df, threshold, prior_class_1_prob):
     full_test_df['is_one_hop'] = full_test_df['hops'] == 1
     full_test_df['is_one_hop'] = full_test_df['is_one_hop'].astype(int)
     
+    specterv1_test_df['is_one_hop'] = specterv1_test_df['hops'] == 1
+    specterv1_test_df['is_one_hop'] = specterv1_test_df['is_one_hop'].astype(int)
+    
     full_test_df['correct'] = full_test_df['is_one_hop'] == full_test_df['pred'] 
     full_test_df['correct'] = full_test_df['correct'].astype(int)
+    
+    specterv1_test_df['correct'] = specterv1_test_df['is_one_hop'] == specterv1_test_df['pred'] 
+    specterv1_test_df['correct'] = specterv1_test_df['correct'].astype(int)
     
     y_true = full_test_df['is_one_hop']
     y_pred = full_test_df['pred'] 
@@ -86,41 +101,106 @@ def test_acc_from_threshold(full_test_df, threshold, prior_class_1_prob):
     
     youdens_j_by_bin = full_test_df.groupby(full_test_df[['bin0', 'bin1']].max(axis=1))\
                           .apply(lambda group: youdens_j_score(group['is_one_hop'], group['pred'])).to_dict()
-    
-    #full_test_df['correct'] = full_test_df.apply(lambda row: 0 if row['no_pred'] else row['correct'], axis=1)
-    
-    
-    # check if class prediciton is right
-    #valid_test_df = full_test_df[full_test_df['cos_sim'] != -1].copy()
-    #valid_test_df['is_one_hop'] = valid_test_df['hops'] == 1
-    #valid_test_df['is_one_hop'] = valid_test_df['is_one_hop'].astype(int)
-    #valid_test_df['correct'] = valid_test_df['is_one_hop'] == valid_test_df['pred']
-    #valid_test_df['correct'] = valid_test_df['correct'].astype(int)
+        
     # calculate accuracy
+    acc_specter = sum(specterv1_test_df['correct'])/len(specterv1_test_df)
     acc = sum(full_test_df['correct'])/len(full_test_df)
+        
     acc_by_bin = full_test_df.groupby(full_test_df[['bin0', 'bin1']].max(axis=1))['correct'].mean().to_dict()
+    size_by_bin = full_test_df.groupby(full_test_df[['bin0', 'bin1']].max(axis=1))['correct'].size().to_dict()
+    
+    # Calculate the maximum of bin0 and bin1
+    max_bin0_bin1 = full_test_df[['bin0', 'bin1']].max(axis=1)
 
-    return acc, acc_by_bin,youdens_j, youdens_j_by_bin,  rate_cos_sim_minus_1
+    # Filter the full_test_df based on the condition
+    filtered_df = full_test_df[(max_bin0_bin1 > test_train_split_bin) & (max_bin0_bin1 <= 100)]
+    
+    acc_forecast = sum(filtered_df['correct'])/len(filtered_df)
+    youdens_j_forecast = youdens_j_score(filtered_df['is_one_hop'].tolist(), filtered_df['pred'].tolist())
+    
+    return acc_forecast, acc_by_bin, youdens_j_forecast, youdens_j_by_bin,  rate_cos_sim_minus_1
+
+
+def is_tuple_in_keep_ids(row, keep_ids):
+    return (row['id0'], row['id1']) in keep_ids
 
     
-# Your function that performs the task for each iteration of the loop
-def process_data(walk_path, output_folder, walk_with_bins_df):
+def process_data(walk_path, output_folder, walk_with_bins_df, specterv1_test_df, specterv1_threshold, keep_ids_df, fallback_method):
     cumbin = int(walk_path.split('/')[-3])
+    if os.path.isfile(f'{output_folder}/{cumbin}_when_necessary_prone.pkl'):
+        print(f'{output_folder}/{cumbin}_when_necessary_prone.pkl exists, skipping...')
+        return True
     test_dfs = sorted([os.path.join(walk_path, f) for f in os.listdir(walk_path) if f[-3] == '.' and f[-2:]!='aa'])
     test_df_list = [pd.read_csv(file, sep='\t', header=None) for file in test_dfs]
     test_df = pd.concat(test_df_list)
+    test_df = df_merge(test_df, walk_with_bins_df)
+
+    test_df = test_df.drop_duplicates(subset=['id0', 'id1'], keep='first')
+
+    # Perform an inner merge between 'test_df' and 'keep_ids_df'
+    test_df = test_df.merge(keep_ids_df, on=['id0', 'id1'], how='inner')
     
 
     train_df = pd.read_csv(os.path.join(walk_path, 'walk12.aa'), sep='\t', header=None) 
     train_df = df_merge(train_df, walk_with_bins_df)
-    test_df = df_merge(test_df, walk_with_bins_df)
     threshold, prior_class_1_prob = clf_threshold_finder(train_df)
-    acc, acc_by_bin, youdens_j, youdens_j_by_bin, rate_cos_sim_minus_1 = test_acc_from_threshold(test_df, threshold, prior_class_1_prob)
+    acc, acc_by_bin, youdens_j, youdens_j_by_bin, rate_cos_sim_minus_1 = test_acc_from_threshold(test_df, threshold, 
+                                                                                                 prior_class_1_prob, cumbin,
+                                                                                                 specterv1_test_df, 
+                                                                                                 specterv1_threshold,
+                                                                                                 fallback_method)
     results = {'acc':acc, 'acc_by_bin':acc_by_bin, 'youdens_j':youdens_j, 
                'youdens_j_by_bin':youdens_j_by_bin,  'rate_cos_sim_minus_1':rate_cos_sim_minus_1}
     joblib.dump(results, f'{output_folder}/{cumbin}_when_necessary_prone.pkl')
 
-   
+# Function to create a tuple from [id0, id1]
+def create_tuple(row):
+    return tuple(row)
+
+
+# Function to remove rows until the 'is_one_hop' ratio is 0.3
+def filter_by_ratio(group):
+    target_ratio = 0.3
+    current_ratio = group['is_one_hop'].mean()
+    drop_amount = max(len(group) // 1000, 1)  # Adjust the drop amount as needed
+    if current_ratio > target_ratio:
+        # If the initial ratio is greater than the target, start by dropping is_one_hop=True rows
+        while current_ratio > target_ratio:
+            rows_to_remove = group[group['is_one_hop'] == True].tail(drop_amount)
+            if not rows_to_remove.empty:
+                group = group.drop(rows_to_remove.index)
+                current_ratio = group['is_one_hop'].mean()
+            else:
+                # If no more rows can be removed, exit the loop
+                break
+    else:
+        # If the initial ratio is smaller than the target, start by dropping is_one_hop=False rows
+        while current_ratio < target_ratio:
+            rows_to_remove = group[group['is_one_hop'] == False].tail(drop_amount)
+            if not rows_to_remove.empty:
+                group = group.drop(rows_to_remove.index)
+                current_ratio = group['is_one_hop'].mean()
+            else:
+                # If no more rows can be removed, exit the loop
+                break
+    
+    return group
+    
+def get_test_ids(test_df):
+    test_df = test_df.drop_duplicates(subset=['id0', 'id1'], keep='first')
+    test_df['is_one_hop'] = test_df['hops'] == 1
+    grouped_test_df = test_df.groupby(test_df[['bin0', 'bin1']].max(axis=1))
+    balanced_test_df = pd.concat([filter_by_ratio(group) for _, group in tqdm(grouped_test_df)])
+
+    # Apply the function to create a new column containing tuples
+    balanced_test_df['id_tuple'] = balanced_test_df[['id0', 'id1']].apply(create_tuple, axis=1)
+
+    # Convert the 'id_tuple' column to a set
+    id_tuple_set = set(balanced_test_df['id_tuple'])
+
+    # Display the resulting set of tuples
+    return id_tuple_set
+    
 def main():
     # Initialize ArgumentParser
     parser = argparse.ArgumentParser(description="Walk Forecasting Script")
@@ -128,8 +208,10 @@ def main():
     # Add arguments
     parser.add_argument("--output", "-o", type=str, default="walk_forecasting/", 
                         help="Output folder to store the walk forecasting results")
+    parser.add_argument("--method", "-m", type=str, default="guess", 
+                        help="Fallback method when Prone has no values for one or more papers. Options: guess, specter1")
     parser.add_argument("--threads", "-t", type=int, default=1, 
-                        help="simultaneous threads to use")
+                        help="Simultaneous threads to use")
     
     # Parse the arguments
     args = parser.parse_args()
@@ -144,12 +226,37 @@ def main():
     walk_with_bins_df = pd.read_csv(walk_with_bins_path, sep='\t', header=None)
     
     walk_path = os.environ.get('walkdir')
+
     cumbins_walks = sorted([os.path.join(walk_path, f, 'walk_pieces/when_necessary') for \
                             f in os.listdir(walk_path) if f[-3:]!='zip'])
-    process_data(cumbins_walks[0], output_folder, walk_with_bins_df)
+
+    
+    ## set up specterv1 model
+    specterv1_walk_path = os.path.join(os.environ.get('JSALTdir'), 'semantic_scholar/p.vickers/specter/walk_pieces/when_necessary')
+    specterv1_test_dfs = sorted([os.path.join(specterv1_walk_path, f) for f in os.listdir(specterv1_walk_path) if f[-3] == '.' and f[-2:]!='aa'])
+    specterv1_test_df_list = [pd.read_csv(file, sep='\t', header=None) for file in specterv1_test_dfs]
+    specterv1_test_df = pd.concat(specterv1_test_df_list)
+    specterv1_test_df = df_merge(specterv1_test_df, walk_with_bins_df)
+    specterv1_test_df = specterv1_test_df.drop_duplicates(subset=['id0', 'id1'], keep='first')
+
+    specterv1_train_df = pd.read_csv(os.path.join(specterv1_walk_path, 'walk12.aa'), sep='\t', header=None) 
+    specterv1_train_df = df_merge(specterv1_train_df, walk_with_bins_df)
+    specterv1_threshold, _ = clf_threshold_finder(specterv1_train_df)
+
+    id_tuple_set = get_test_ids(specterv1_test_df)
+    
+    keep_ids_df = pd.DataFrame(list(id_tuple_set), columns=['id0', 'id1'])
+
+    # Perform an inner merge between 'test_df' and 'keep_ids_df'
+    specterv1_test_df = specterv1_test_df.merge(keep_ids_df, on=['id0', 'id1'], how='inner')
+
     num_threads = args.threads
+
     with concurrent.futures.ThreadPoolExecutor(max_workers=num_threads) as executor:
-        futures = [executor.submit(process_data, data, output_folder, walk_with_bins_df) for data in cumbins_walks]
+        futures = [executor.submit(process_data, walk_path, 
+                                   output_folder, walk_with_bins_df, 
+                                   specterv1_test_df, specterv1_threshold, 
+                                   keep_ids_df, args.method) for walk_path in cumbins_walks]
 
         for _ in tqdm(concurrent.futures.as_completed(futures), total=len(cumbins_walks)):
             pass
--- src/forecasting/Random_Walks_Plot.py
+++ src/forecasting/Random_Walks_Plot.py
@@ -8,8 +8,12 @@ import os
 from tqdm import tqdm
 import argparse
 import pandas as pd
+from copy import deepcopy
+from collections import defaultdict
 
 import matplotlib.pyplot as plt
+from matplotlib.colors import Normalize
+
 import seaborn as sns
 
 
@@ -20,9 +24,13 @@ def results_to_plot(results_folder, output_folder):
     bin_to_overall_accs = {}
     bin_to_overall_youdens_j = {}
 
+    print(os.listdir(results_folder))
     for file_name in tqdm(sorted(os.listdir(results_folder), key=lambda x:int(x.split('_')[0]))):
+        
         results_file_path = os.path.join(results_folder, file_name)
         cumbin = int(file_name.split('_')[0])
+        if cumbin in {30, 31}:
+            continue
 
         try:
             r = joblib.load(results_file_path)
@@ -42,36 +50,62 @@ def results_to_plot(results_folder, output_folder):
 
         except:
             print(f'failed with {file}')
-            
+    
+    
     df = pd.DataFrame(R_acc)
+    
     df.to_csv(os.path.join(output_folder, f'acc_preds_prone_when_necessary.tsv'), sep='\t')
-            
+    
+    plot_forecast_from_cumbins(bin_to_overall_metric=R_acc, y_axis_labels=y_axis_labels, 
+                               metric='Accuracy',
+                               first_plot_filename=os.path.join(output_folder, f'ProNE_when_necessary_acc_forecast.jpg'),
+                               second_plot_filename=os.path.join(output_folder, f'ProNE_when_necessary_cumbin_effects.jpg'),
+                               min_forecast_steps=5)
+    
     plot_forecast_heatmap(R=R_acc, metric='Accuracy', y_axis_labels=y_axis_labels,
                           output_filename=os.path.join(output_folder, f'ProNE_when_necessary_acc_heatmap.jpg'))
     plot_forecast_heatmap(R=R_youdens_j, metric='Youden\'s_J', y_axis_labels=y_axis_labels,
                           output_filename=os.path.join(output_folder, f'ProNE_when_necessary_youdensj_heatmap.jpg'))
     plot_forecast_linegraph(bin_to_overall_metric=bin_to_overall_accs, metric='Accuracy', output_filename=os.path.join(output_folder, f'ProNE_when_necessary_acc_lineplot.jpg'))
     plot_forecast_linegraph(bin_to_overall_metric=bin_to_overall_youdens_j, metric='Youden\'s J', output_filename=os.path.join(output_folder, f'ProNE_when_necessary_youdensj_lineplot.jpg'))
+    
+    
 
 
 def plot_forecast_heatmap(R, metric, y_axis_labels, output_filename):
-    plt.figure(figsize=(12,12), dpi=600)
-    sns.set(font_scale = 1.1)
+    plt.figure(figsize=(8,8), dpi=300)
+    sns.set(font_scale = 1.4)
+    
+    min_metric_val = np.min(R)
+    
+    R_diag = []
+    for row_idx, r_row in enumerate(R):
+        new_row = deepcopy(r_row)
+        new_row[y_axis_labels[row_idx]] = min_metric_val
+        R_diag.append(new_row)
+        
+    percentile_80 = np.percentile(R_diag, 98)
 
+    # Create the heatmap with adjusted color scaling
+    #ax = sns.heatmap(R_diag, annot=False, cmap='viridis', vmin=0, vmax=vmax_value)
 
-    ax = sns.heatmap(R, annot=False)
-    ax.set_yticklabels(y_axis_labels, rotation = 0)
-    ax.set_xticklabels(np.arange(0,100,3), rotation = -90)
+    ax = sns.heatmap(R_diag, annot=False, cmap='viridis', vmax=percentile_80, robust=True)
 
-    ax.set_title(f"{metric} of ProNE Cumulative Bin Models for 1 vs 2-4 Hop Walk Prediction per Prediciton Bin ", fontsize=14, pad=20)
-    ax.set_xlabel(f"Bin {metric}", fontsize=14, labelpad=20)
+    y_ticks = plt.yticks()[0]
+    
+    yticklabels = [y_axis_labels[int(round(i))] for i in y_ticks]
+    ax.set_yticklabels(yticklabels, rotation = 0)
+    ax.set_xticklabels(ax.get_xticklabels(), rotation=90)
+
+    ax.set_title(f"{metric} of ProNE Bins for 1 vs 2-4 Hop Walk Prediction", fontsize=14, pad=20)
+    ax.set_xlabel(f"Evaluation Bin", fontsize=14, labelpad=20)
     ax.set_ylabel("Max Train Bin", fontsize=14, labelpad=20)
     plt.savefig(output_filename)
     
 
 def plot_forecast_linegraph(bin_to_overall_metric, metric, output_filename):
 
-    plt.figure(figsize=(8,8), dpi=600)
+    plt.figure(figsize=(30, 8), dpi=300)
 
     x_values = list(bin_to_overall_metric.keys())
     y_values = list(bin_to_overall_metric.values())
@@ -82,8 +116,59 @@ def plot_forecast_linegraph(bin_to_overall_metric, metric, output_filename):
     plt.xlabel('Max Train Bin')
     plt.title(f'{metric} of ProNE Cumulative Bin Models for 1 vs 2-4 Hop Walk Prediction', fontsize=14, pad=20)
 
+    # Set all x-values as ticks
+    plt.xticks(x_values, rotation=90)
+    # Add a grid for every x-tick
+    plt.grid(axis='x', linestyle='--')
+
     plt.savefig(output_filename)
 
+import matplotlib.pyplot as plt
+from collections import defaultdict
+
+def plot_forecast_from_cumbins(bin_to_overall_metric, y_axis_labels, metric, min_forecast_steps=5, first_plot_filename=None, second_plot_filename=None):
+    bins_out = defaultdict(list)
+    cumbins = []
+    trainlen_out_accs = defaultdict(list)
+
+    # Iterate through y_axis_labels and bin_to_overall_metric
+    for cumbin, row in zip(y_axis_labels, bin_to_overall_metric):
+        if cumbin + min_forecast_steps > 100:
+            continue
+        cumbins.append(cumbin)
+        # Calculate forecasts and training lengths
+        for forecast in range(cumbin + 1, 100):
+            bins_out[forecast - cumbin].append(row[forecast])
+            trainlen_out_accs[cumbin] = [row[idx] for idx in range(len(row)) if cumbin < idx < 99]
+
+    data_for_boxplot = [bins_out[key] for key in bins_out]
+
+    # Create the first box plot
+    plt.figure(figsize=(8, 4), dpi=600)
+    plt.boxplot(data_for_boxplot, labels=bins_out.keys())
+
+    # Add labels and title for the first plot
+    plt.ylabel(f'Forecast {metric}')
+    plt.xlabel('Num Bins Forecasting Forward')
+    plt.title(f'Performance Forecasting Range Averaged Across all Cumbins vs {metric}')
+    plt.xticks(range(1, len(bins_out.keys()) + 1, 3), list(bins_out.keys())[::3], rotation=90)
+    plt.savefig(first_plot_filename)
+
+    # Create the second box plot
+    data_for_boxplot = [trainlen_out_accs[key] for key in trainlen_out_accs]
+    plt.clf()
+    plt.figure(figsize=(8, 4), dpi=600)
+    plt.boxplot(data_for_boxplot, labels=trainlen_out_accs.keys())
+
+    # Add labels and title for the second plot
+    plt.ylabel(f'Forecast {metric}')
+    plt.xlabel('Num Training Bins')
+    plt.title(f'Num Training Bins vs Forecast {metric}')
+    plt.xticks(range(1, len(trainlen_out_accs.keys()) + 1, 3), list(trainlen_out_accs.keys())[::3], rotation=90)
+    plt.savefig(second_plot_filename)
+
+# Example usage:
+# plot_forecast_from_cumbins(bin_to_overall_metric, y_axis_labels, 'Accuracy', 'forecast_plot.png', min_forecast_steps=5, second_plot_filename='training_plot.png')
 
 def main():
     # Initialize ArgumentParser
