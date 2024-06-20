2979bbe00f6a81ff493d79aea5ff437170fe857d
Kenneth Church <kwchurch@us.ibm.com>
2024-06-20 05:40:47 -0400

    summary stats

---
+++ src/parse_s2_database/papers_to_sources_with_ids.py
@@ -0,0 +1,28 @@
+#!/usr/bin/env python
+
+import sys,json,os,gzip
+
+errors=0
+
+def my_get(rec, keys):
+    if len(keys) == 0:
+        return rec
+    elif isinstance(rec, dict) and keys[0] in rec:
+        return my_get(rec[keys[0]], keys[1:])
+
+for f in sys.argv[1:]:
+    with gzip.open(f) as fd:
+        for line in fd:
+            rline = line.rstrip()
+            if len(rline) > 1:
+                j = json.loads(rline)
+                cid = my_get(j, ['corpusid'])
+                ids = my_get(j, ['externalids'])
+                if ids is None: continue
+                sources = [ str(k) + ':' + str(ids[k]) for k in ids if not ids[k] is None ]
+                print(str(cid) + '\t' + '\t'.join(map(str, sources)))
+
+print(str(errors) + ' errors', file=sys.stderr)
+
+
+
--- src/summary_stats/citations_by_papers_and_years.py
+++ src/summary_stats/citations_by_papers_and_years.py
@@ -4,11 +4,12 @@ import numpy as np
 import sys,argparse
 
 parser = argparse.ArgumentParser()
-parser.add_argument("--graph", help="citation graph (*.X.i, *.Y.i)", required=True)
+parser.add_argument("-G", "--graph", help="citation graph (*.X.i, *.Y.i)", required=True)
 parser.add_argument("--year", help="*npy file", required=True)
 parser.add_argument("--max_year", type=int, help="2025", default=2025)
-parser.add_argument("--publication_year", type=int, help="2000", default=2000)
-
+parser.add_argument("--publication_year", type=int, help="2000", default=None)
+parser.add_argument("--filter", help="txt file containing list of corpus ids", default=None)
+parser.add_argument("--flush", action='store_true')
 args = parser.parse_args()
 
 year = np.load(args.year)
@@ -19,14 +20,30 @@ Y=np.fromfile(args.graph + '.Y.i', dtype=np.int32)
 N = len(year)
 # res=np.zeros((N, args.max_year - args.publication_year + 1, dtype=np.int32)
 
+filter=None
+if not args.filter is None:
+    ids = np.loadtxt(args.filter, dtype=np.int32)
+    filter = dict(zip(ids,ids))
+
 Xyear= year[X]
 Yyear= year[Y]
-S = Yyear == args.publication_year
 
-corpusids = X[S]
-gap = Yyear[S] - args.publication_year
+if not args.publication_year is None:
+    S = Yyear == args.publication_year
 
-for id,g in zip(corpusids, gap):
-    print(str(id) + '\t' + str(g))
+    for id2,gap in zip(Y[S], Xyear[S] - args.publication_year):
+        if filter is None or id2 in filter:
+            print('\t'.join(map(str,[id2, gap])))
+            if args.flush: sys.stdout.flush()
+
+else:
+    for id2,x,y in zip(Y, Xyear, Yyear):
+        if filter is None or id2 in filter:
+            print('\t'.join(map(str,[id2, x, y])))
+            if args.flush: sys.stdout.flush()
+    
+
+# for id1,id2,y1,y2 in zip(X, Y, Xyear, Yyear):
+#     print('\t'.join(map(str,[id1,id2, y1, y2])))
 
 
